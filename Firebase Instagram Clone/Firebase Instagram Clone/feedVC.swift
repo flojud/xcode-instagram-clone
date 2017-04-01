d8352d8b6e52a544cbfa0724318aab259c08bb55
Florian Jud <florian@famjud.de>
2017-04-01 17:18:13 +0200

    functional test

--- Firebase Instagram Clone/Firebase Instagram Clone/feedVC.swift
+++ Firebase Instagram Clone/Firebase Instagram Clone/feedVC.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseDatabase
+import SDWebImage
 
 class feedVC: UIViewController, UINavigationControllerDelegate, UITableViewDelegate, UITableViewDataSource{
 
@@ -14,25 +19,57 @@ class feedVC: UIViewController, UINavigationControllerDelegate, UITableViewDeleg
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        getDataFromServer()
     }
     
+    //Fetch data from Firebaseserver
+    var userNameArray = [String]()
+    var postTextArray = [String]()
+    var postImageURLArray = [String]()
     
-    //Define the numbers or rows
+    func getDataFromServer(){
+        FIRDatabase.database().reference().child("users").observe(FIRDataEventType.childAdded, with: { (snapshot) in
+            
+            //Parse JSON Response from Database
+            let values = snapshot.value! as! NSDictionary
+            let posts = values["posts"] as! NSDictionary
+            let postIDs = posts.allKeys
+            
+            for id in postIDs {
+                let singlePost = posts[id] as! NSDictionary
+                self.userNameArray.append(singlePost["postedby"] as! String)
+                self.postTextArray.append(singlePost["posttext"] as! String)
+                self.postImageURLArray.append(singlePost["image"] as! String)
+            }
+            
+            self.tableView.reloadData()
+            
+        })
+    }
+    
+    //Number of rows in tableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return userNameArray.count
     }
     
-    //Get the content for the tableView rows
+    //Assign the labels and images with the fetched content
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! feedCell
         
-        
+        cell.postText.text = postTextArray[indexPath.row]
+        cell.userName.text = userNameArray[indexPath.row]
+        cell.postImage.sd_setImage(with: URL(string: self.postImageURLArray[indexPath.row]))
+      
         return cell
     }
+
     
-    
-     //Logout function and go back to signup VC
+    //Logout function and go back to signup VC
     @IBAction func logoutButtonClicked(_ sender: Any) {
         UserDefaults.standard.removeObject(forKey: "usersigned")
         UserDefaults.standard.synchronize()
